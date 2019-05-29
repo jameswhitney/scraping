78b930d9d10977f1906e5c4aa6c2b5fff00894b4
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2019-05-29 12:04:57 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scraping Website Practice
+This simple python script uses the requests libray and Beautiful Soup to
+scrape the website [https://www.thegoldbugs.com] any hidden messages. 
+This script is part of a project from Jose Portilla's 'Complete Python 3 Masterclass Journey'
+and is used for educational purposes only.
---
+++ scraping.py
@@ -0,0 +1,20 @@
+
+###############################################
+# This script is used to scrape a test        #
+# website and decrypt any messages contained  #
+# within the html of the site                 #
+###############################################
+
+
+# import libraries needed to simplify the scraping process
+import requests
+import bs4
+
+
+# Use methods within the requests library to obtain html from https://www.thegoldbugs.com
+# Before making the request check to see what endpoint contains the encrypted message.
+# Once the proper html is recieved via a get request use the Beautiful Soup library
+# to parse the HTML. It is neccessary to view the website's source code to
+# determine which tag, tags or classes which encapsulte the encrypted message. After the appropriate tags
+# are found, use select method in Beautiful Soup to extract any data within the tags or CSS classes.
+# Finally determine how to decrypt the message based on information found within the site.
